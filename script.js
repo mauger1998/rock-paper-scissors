285a3f59b6f19f6921f63e53d8fd77a32ead9623
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-09-26 10:19:22 +0100

    Make playerSelection always lower case

--- script.js
+++ script.js
@@ -17,24 +17,31 @@ let  getComputerChoice = () => {
 
 
 const playRound = (playerSelection, computerSelection) => {
+    playerSelection = playerSelection.toLowerCase();
     if (playerSelection === "rock" && computerSelection === "rock"){
         return "you tied you both picked rock";
+
     } else if (playerSelection === "paper" && computerSelection === "paper"){
         return "you tied you both picked paper";
+
     } else if (playerSelection === "scissors" && computerSelection === "scissors"){
         return "you tied you both picked scissors";
 
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         return "you lost rock beats scissors";
+
     } else if (playerSelection === "scissors" && computerSelection === "paper"){
         return "you won scissors beats paper";
+
     } else if (playerSelection === "rock" && computerSelection === "paper") { 
         return "you lost paper beats rock";
 
     } else if (playerSelection === "rock" && computerSelection === "scissors") { 
         return "you won rock beats scissors";
+
     } else if (playerSelection === "paper" && computerSelection === "scissors") { 
-        return "you lost scissors beats paper";  
+        return "you lost scissors beats paper";
+          
     } else if (playerSelection === "paper" && computerSelection === "rock") { 
         return "you won paper beats rock";
     }
